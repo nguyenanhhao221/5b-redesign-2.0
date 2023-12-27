358b2a245ac07b2dcde55b5645e614d85d014aaa
Hao Nguyen <hao@haonguyen.tech>
2023-12-27 23:28:06 +0700

    feat: add shadcn ui packages

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,8 @@ const { fontFamily } = require("tailwindcss/defaultTheme");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: ["class"],
+  prefix: "",
   theme: {
     aspectRatio: {
       auto: "auto",
@@ -29,6 +31,13 @@ module.exports = {
     corePlugins: {
       aspectRatio: false,
     },
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
       colors: {
         "5b-yellow": "#ffb400",
@@ -39,7 +48,24 @@ module.exports = {
       fontFamily: {
         sans: ["var(--font-inter)", ...fontFamily.sans],
       },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
     },
   },
-  plugins: [require("@tailwindcss/aspect-ratio")],
+  plugins: [
+    require("tailwindcss-animate"),
+    require("@tailwindcss/aspect-ratio"),
+  ],
 };
